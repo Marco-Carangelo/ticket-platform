65f770e65ac7de99ca423975efd30d73422cfb3b
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-26 20:26:07 +0200

    fix: repository name fixed

--- src/main/java/org/lesson/java/spring/TicketPlatformApplication.java
+++ src/main/java/org/lesson/java/spring/TicketPlatformApplication.java
@@ -1,4 +1,4 @@
-package org.lesson.java.spirng;
+package org.lesson.java.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
