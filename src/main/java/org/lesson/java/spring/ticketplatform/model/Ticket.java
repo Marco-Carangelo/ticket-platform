f9d44259dc972bef10fb22e2f80df482fc9f2a13
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-26 20:55:13 +0200

    update:  status enum object uptated to work on string values

--- src/main/java/org/lesson/java/spring/ticketplatform/model/Ticket.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/model/Ticket.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,6 +34,7 @@ public class Ticket {
 	
 	
 	@Column
+	@Enumerated(EnumType.STRING)
 	private Status ticketStatus = Status.OPEN;
 	
 	@NotNull
