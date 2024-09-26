13077e3c23d2eee4109b9ecdd9103264a57f9e0e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-26 22:03:24 +0200

    add: findTicket method that return a list of Ticket

--- src/main/java/org/lesson/java/spring/ticketplatform/service/TicketService.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/service/TicketService.java
@@ -1,5 +1,8 @@
 package org.lesson.java.spring.ticketplatform.service;
 
+import java.util.List;
+
+import org.lesson.java.spring.ticketplatform.model.Ticket;
 import org.lesson.java.spring.ticketplatform.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,10 @@ public class TicketService {
 
 	@Autowired
 	private TicketRepository repository;
+	
+	public List<Ticket> findTickets(){
+		
+		return repository.findAll();
+	}
 	
 }
