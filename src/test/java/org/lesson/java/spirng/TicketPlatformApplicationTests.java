6e54d5358298b5c8435bfa71961406aa31ad468d
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-26 17:25:58 +0200

    Initial commit

---
+++ src/test/java/org/lesson/java/spirng/TicketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.java.spirng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
