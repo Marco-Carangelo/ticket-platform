fa59d7537b890a430236cff2a05fba4be0d811e3
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-26 17:55:31 +0200

    add: new model class Ticket

---
+++ src/main/java/org/lesson/java/ticketplatform/model/Ticket.java
@@ -0,0 +1,11 @@
+package org.lesson.java.ticketplatform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tickets")
+public class Ticket {
+	
+
+}
