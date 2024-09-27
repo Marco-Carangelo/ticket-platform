14da3d5dad4a8b9b16b6173028bb695b5da339b9
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-27 17:18:16 +0200

    add: repository custom query method
    New method findByTicketHeaderContainsIgnoreCase  used to find Ticket elements by ther header

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/TicketRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/TicketRepository.java
@@ -1,8 +1,14 @@
 package org.lesson.java.spring.ticketplatform.repository;
 
+import java.util.List;
+
 import org.lesson.java.spring.ticketplatform.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer>  {
-
+	
+	public List<Ticket> findByTicketHeaderContainsIgnoreCase(String text);
+		
+		
+	
 }
