8c9d9e0db01f31346177c1d7e1b79ffcfd4fb89b
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-27 17:21:18 +0200

    update: index method updated
    Method updated to optionally find elements based on the search of header text

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/TicketController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/TicketController.java
@@ -1,11 +1,16 @@
 package org.lesson.java.spring.ticketplatform.controller;
 
+import java.util.List;
+
+import org.lesson.java.spring.ticketplatform.model.Ticket;
 import org.lesson.java.spring.ticketplatform.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/tickets")
@@ -14,12 +19,22 @@ public class TicketController {
 	@Autowired
 	private TicketService ticketService;
 	
+	
+	// Index method
 	@GetMapping
-	public String index(Model model) {
+	public String index(Model model, @RequestParam( name = "searchedText", required = false ) String searchedText) {
 		
-		model.addAttribute("ticketList", ticketService.findTickets());
-		
+		if( searchedText != null && !searchedText.isEmpty()) {
+			
+			model.addAttribute("ticketList", ticketService.findTicketsContains(searchedText));
+		}else {
+			
+			model.addAttribute("ticketList", ticketService.findTickets());
+			
+		}
 		return "/tickets/index";
 	}
+	
+	
 
 }
