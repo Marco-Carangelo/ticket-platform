d0feafc441269db76232615279228989b338144d
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-27 14:30:10 +0200

    add: index method to TicketController

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/TicketController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/TicketController.java
@@ -3,6 +3,8 @@ package org.lesson.java.spring.ticketplatform.controller;
 import org.lesson.java.spring.ticketplatform.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +14,12 @@ public class TicketController {
 	@Autowired
 	private TicketService ticketService;
 	
-	
+	@GetMapping
+	public String index(Model model) {
+		
+		model.addAttribute("ticketList", ticketService.findTickets());
+		
+		return "/tickets/index";
+	}
 
 }
