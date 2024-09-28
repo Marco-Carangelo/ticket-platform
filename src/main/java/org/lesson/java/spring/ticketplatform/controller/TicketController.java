18f6b8978658e9ab2732bf285ed7e8bb95bcc758
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-28 13:57:11 +0200

    add: new store method to save a new Ticket

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/TicketController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/TicketController.java
@@ -1,16 +1,23 @@
 package org.lesson.java.spring.ticketplatform.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.lesson.java.spring.ticketplatform.model.Ticket;
+import org.lesson.java.spring.ticketplatform.model.Ticket.Status;
 import org.lesson.java.spring.ticketplatform.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/tickets")
@@ -54,5 +61,23 @@ public class TicketController {
 		return "/tickets/create";
 		
 	}
+	
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("ticket") Ticket formTicket,
+			BindingResult bindingResult,
+			Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "/tickets/create";
+		}
+		
+		formTicket.setTicketStatus(Status.OPEN);
+		formTicket.setCreatedAt(LocalDateTime.now());
+		formTicket.setUpdatedAt(LocalDateTime.now());
+		ticketService.createTicket(formTicket);
+		
+		return "redirect:/tickets";
+	}
+	
 
 }
