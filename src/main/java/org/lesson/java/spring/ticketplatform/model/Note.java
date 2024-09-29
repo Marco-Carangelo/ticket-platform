fd26c5bcc874238b0b58018ecd4a22b5f4320b1a
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-29 19:49:43 +0200

    add: class fields and validation annotation

--- src/main/java/org/lesson/java/spring/ticketplatform/model/Note.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/model/Note.java
@@ -1,12 +1,46 @@
 package org.lesson.java.spring.ticketplatform.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "notes")
 public class Note {
 	
-	
+	//Definition of the class fields
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer id;
+		
+		@Size(min = 2, max = 50)
+		@NotBlank
+		@Column
+		private String author;
+		
+		@Size(min = 10, max = 500)
+		@NotBlank
+		@Column
+		private String noteText;
+		
+		@NotNull
+		@Column
+		private LocalDateTime createdAt;
+		
+		@ManyToOne
+		@JoinColumn(name="ticket_id", nullable=false)
+		private Ticket ticket;
+		
+		
 
 }
