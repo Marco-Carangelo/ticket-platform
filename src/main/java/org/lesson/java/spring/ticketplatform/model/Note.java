b1c866d7a124e1ebc9b8b186c312e6c855fa8619
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-29 19:44:23 +0200

    add: new model class Note

---
+++ src/main/java/org/lesson/java/spring/ticketplatform/model/Note.java
@@ -0,0 +1,12 @@
+package org.lesson.java.spring.ticketplatform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+	
+	
+
+}
