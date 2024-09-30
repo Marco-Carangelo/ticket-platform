7ad1f6dafe046f63c1e117051376590611df896e
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-01 01:24:58 +0200

    fix: wrong import fixed

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/CategoryRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package org.lesson.java.spring.ticketplatform.repository;
 
-import java.util.Locale.Category;
+
+import org.lesson.java.spring.ticketplatform.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
