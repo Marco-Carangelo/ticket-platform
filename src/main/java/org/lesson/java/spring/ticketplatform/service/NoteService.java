5745525c899d0ded82a6bc59c0eed0632f2f0043
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-09-30 19:23:05 +0200

    add: new method createNote in NoteRepository

--- src/main/java/org/lesson/java/spring/ticketplatform/service/NoteService.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/service/NoteService.java
@@ -1,5 +1,6 @@
 package org.lesson.java.spring.ticketplatform.service;
 
+import org.lesson.java.spring.ticketplatform.model.Note;
 import org.lesson.java.spring.ticketplatform.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,10 @@ public class NoteService {
 	
 	@Autowired
 	private NoteRepository repository;
+	
+	public Note createNote(Note note) {
+		
+		return repository.save(note);
+	}
 
 }
