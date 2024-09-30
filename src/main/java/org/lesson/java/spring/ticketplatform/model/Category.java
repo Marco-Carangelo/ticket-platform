f3ba43c088498cc784baf7a4e52a651d9423c437
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-01 00:39:29 +0200

    add: new model class Category

---
+++ src/main/java/org/lesson/java/spring/ticketplatform/model/Category.java
@@ -0,0 +1,10 @@
+package org.lesson.java.spring.ticketplatform.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+}
