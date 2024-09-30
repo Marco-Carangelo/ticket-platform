dc1bb34fe03bf236e0d25580ace4c9f16227fd92
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-01 00:47:34 +0200

    add: category fields and jpa annotation

--- src/main/java/org/lesson/java/spring/ticketplatform/model/Category.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/model/Category.java
@@ -1,10 +1,35 @@
 package org.lesson.java.spring.ticketplatform.model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "categories")
 public class Category {
 
+	//Definition of the class fields
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Integer id;
+		
+		@Size(min = 2, max = 50)
+		@Column
+		private String name;
+		
+		@Size(min = 10, max = 150)
+		@NotBlank
+		@Column
+		private String description;
+		
+		@ManyToMany(mappedBy = "categories")
+		private List<Ticket> tickets;
 }
