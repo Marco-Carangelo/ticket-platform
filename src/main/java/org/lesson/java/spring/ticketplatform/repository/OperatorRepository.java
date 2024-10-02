389e1e06b1d589595adfc7583c88642b73302bc9
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-02 23:57:43 +0200

    add: custom jpa query method to find active operators

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/OperatorRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/OperatorRepository.java
@@ -1,8 +1,12 @@
 package org.lesson.java.spring.ticketplatform.repository;
 
+import java.util.List;
+
 import org.lesson.java.spring.ticketplatform.model.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OperatorRepository extends JpaRepository<Operator , Integer>   {
+	
+	public List<Operator> findByInactiveFalse();
 
 }
