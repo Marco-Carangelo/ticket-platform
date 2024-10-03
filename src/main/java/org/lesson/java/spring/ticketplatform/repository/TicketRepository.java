34598ad246becd7d944de60ddc474158c8846041
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-03 19:04:04 +0200

    add: show method for individual operator details

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/TicketRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/TicketRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TicketRepository extends JpaRepository<Ticket, Integer>  {
 	
 	public List<Ticket> findByTicketHeaderContainsIgnoreCase(String text);
-		
+	
 		
 	
 }
