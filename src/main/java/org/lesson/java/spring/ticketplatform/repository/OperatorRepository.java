4db7956158723a8ef5955c448c43e60d74382961
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-03 16:52:14 +0200

    add: new custom query method to find Operator by user_id foreign key

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/OperatorRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/OperatorRepository.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import org.lesson.java.spring.ticketplatform.model.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OperatorRepository extends JpaRepository<Operator , Integer>   {
 	
 	public List<Operator> findByInactiveFalse();
+	
+	@Query("FROM Operator o WHERE o.user.id = :userId")
+	public Operator findByUser(@Param("userId") Integer userId);
 
 }
