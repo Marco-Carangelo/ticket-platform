40631df2e994f456cd185129009e82e8467e9b7f
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-04 22:27:06 +0200

    add: new method findByUsername

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/UserRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package org.lesson.java.spring.ticketplatform.repository;
 
+import java.util.Optional;
+
 import org.lesson.java.spring.ticketplatform.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+	
+	public Optional<User> findByUsername(String username);
 
 	
 }
