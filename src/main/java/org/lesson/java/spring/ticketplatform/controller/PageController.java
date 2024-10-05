3cc74e9624cd6e9748f6d62ad24ab2efb9a4b8db
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-05 18:03:52 +0200

    add: new method home the redirect the user after login

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/PageController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/PageController.java
@@ -1,8 +1,14 @@
 package org.lesson.java.spring.ticketplatform.controller;
 
+import java.util.Set;
+
 import org.lesson.java.spring.ticketplatform.model.Operator;
+import org.lesson.java.spring.ticketplatform.model.Role;
+import org.lesson.java.spring.ticketplatform.model.User;
 import org.lesson.java.spring.ticketplatform.service.OperatorService;
+import org.lesson.java.spring.ticketplatform.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +22,29 @@ public class PageController {
 	@Autowired
 	private OperatorService operatorService;
 	
-	// Show method
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public String home(Authentication authentication, Model model) {
+		
+		String username = authentication.getName();
+		User user = userService.findUserByUsarname(username).get(); 
+		Set<Role> roles = user.getRoles();
+		
+		
+		for(Role r : roles ) {
+			if (r.getName().equals("ADMIN"))
+				return "redirect:/tickets";
+		}
+		
+		return "redirect:/operators/" + user.getId();
+	
+	}
+	
+	
+	
+
 	@GetMapping("/operators/{id}")
 	public String show(Model model, @PathVariable("id") Integer id) {
 		
