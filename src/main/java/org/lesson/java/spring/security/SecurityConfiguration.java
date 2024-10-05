144f373e1b4ecf88b86d580ceef68719ee5d0b40
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-05 18:02:05 +0200

    add: new configuration class SecurityConfiguration

---
+++ src/main/java/org/lesson/java/spring/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package org.lesson.java.spring.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+	
+	@Bean
+	@SuppressWarnings("removal")
+		SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+			.requestMatchers("/tickets").hasAuthority("ADMIN")
+			.requestMatchers("/operators/**").hasAnyAuthority("OPERATOR","ADMIN")
+			.requestMatchers("/**").permitAll()
+			.and().formLogin()
+			.and().logout()
+			.and().csrf().disable();
+		
+		return http.build();
+	}
+	
+	
+	
+	@Bean
+	DatabaseUserDetailsService userDetailsService() {
+		return new DatabaseUserDetailsService();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+	@Bean
+	DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+		
+		return authProvider;
+	}
+
+}
