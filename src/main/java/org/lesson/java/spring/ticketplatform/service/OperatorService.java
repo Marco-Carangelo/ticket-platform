41215e5933bdea9b96148ca57bffd27b90e566a2
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-07 00:18:20 +0200

    delete: findActiveOperators method no more used

--- src/main/java/org/lesson/java/spring/ticketplatform/service/OperatorService.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/service/OperatorService.java
@@ -13,15 +13,16 @@ public class OperatorService {
 	@Autowired
 	private OperatorRepository repository;
 	
-	public List<Operator> findActiveOperators(){
-		
-		return repository.findByInactiveFalse();
-	}
 	
 	public Operator getOperatorByUserId(Integer userId) {
 		
 		return repository.findByUser(userId);
 		
+	}
+	
+	public List<Operator> findOperators(){
+		
+		return repository.findAll();
 	}
 	
 	
