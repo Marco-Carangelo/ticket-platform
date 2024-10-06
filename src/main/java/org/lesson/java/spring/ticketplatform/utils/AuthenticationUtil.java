d05f1c8042259add31a9eb19170ebe5339d2d845
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-06 22:51:21 +0200

    add: new utility class AuthenticationUtil

---
+++ src/main/java/org/lesson/java/spring/ticketplatform/utils/AuthenticationUtil.java
@@ -0,0 +1,16 @@
+package org.lesson.java.spring.ticketplatform.utils;
+
+import org.lesson.java.spring.ticketplatform.model.User;
+import org.lesson.java.spring.ticketplatform.service.UserService;
+import org.springframework.security.core.Authentication;
+
+public class AuthenticationUtil {
+	
+	//Method to get the currently logged user entity
+	public static User getCurrentUser(Authentication authentication, UserService userService ){
+		
+		String username = authentication.getName();
+		return userService.findUserByUsarname(username).get(); 
+	}
+	
+}
