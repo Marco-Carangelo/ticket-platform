4e63fcf9cbc2bdc7522d09f2fa62598a35d8a0fd
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-07 02:02:43 +0200

    add: findByActiveFalse method to find active operators

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/OperatorRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/OperatorRepository.java
@@ -1,5 +1,7 @@
 package org.lesson.java.spring.ticketplatform.repository;
 
+import java.util.List;
+
 import org.lesson.java.spring.ticketplatform.model.Operator;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface OperatorRepository extends JpaRepository<Operator , Integer>   {
 	
+	public List<Operator> findByInactiveFalse();
 	
 	@Query("FROM Operator o WHERE o.user.id = :userId")
 	public Operator findByUser(@Param("userId") Integer userId);
