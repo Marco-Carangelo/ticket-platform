50b620772c64a81d13ed826dcb1a8bd0cec0f9ac
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-08 22:30:44 +0200

    add: index method to show all the existing tickets

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/api/TicketRestController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/api/TicketRestController.java
@@ -1,6 +1,12 @@
 package org.lesson.java.spring.ticketplatform.controller.api;
 
+import java.util.List;
+
+import org.lesson.java.spring.ticketplatform.model.Ticket;
+import org.lesson.java.spring.ticketplatform.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping("/api/tickets")
 public class TicketRestController {
+	
+	@Autowired
+	 private TicketService ticketService;
+	
+	//This method returns all the existing tickets
+	@GetMapping
+	public List<Ticket> index(){
+		
+		return ticketService.findTickets();
+	}
+	
+	
 
 }
