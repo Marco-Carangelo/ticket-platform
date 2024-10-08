4cea1178ff4635ecc092c3737c62d7612e2b0440
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-08 23:48:26 +0200

    add: new method to find tickets by status

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/api/TicketRestController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/api/TicketRestController.java
@@ -3,10 +3,12 @@ package org.lesson.java.spring.ticketplatform.controller.api;
 import java.util.List;
 
 import org.lesson.java.spring.ticketplatform.model.Ticket;
+import org.lesson.java.spring.ticketplatform.model.Ticket.Status;
 import org.lesson.java.spring.ticketplatform.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +24,20 @@ public class TicketRestController {
 	@GetMapping
 	public List<Ticket> index(){
 		
-		return ticketService.findTickets();
+		List<Ticket> result = ticketService.findTickets();
+		
+		return result;
+	}
+	
+	
+	@GetMapping("/{status}")
+	public List<Ticket> getTicketsByStatus(@PathVariable("status") String requiredStatus){
+		
+		
+		
+		List<Ticket> result = ticketService.findTickets();
+		
+		return result;
 	}
 	
 	
