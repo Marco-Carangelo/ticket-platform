ab354104aaeb26ebe4a2bf6b6c5d94cd23601b89
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-08 19:55:30 +0200

    add: new rest controller TicketsRestController

---
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/api/TicketRestController.java
@@ -0,0 +1,12 @@
+package org.lesson.java.spring.ticketplatform.controller.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/tickets")
+public class TicketRestController {
+
+}
