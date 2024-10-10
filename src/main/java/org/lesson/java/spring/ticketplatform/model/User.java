1891d13e71917c60f5090cacc0bfddf548c59668
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-11 01:23:03 +0200

    add: @GeneratedValue annotation to id

--- src/main/java/org/lesson/java/spring/ticketplatform/model/User.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/model/User.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.Email;
@@ -16,6 +18,7 @@ import jakarta.validation.constraints.Size;
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@NotNull
