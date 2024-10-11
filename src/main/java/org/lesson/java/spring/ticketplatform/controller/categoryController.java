a7d2b2cdfb524874317b992524654c57d0f25bc5
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-12 00:00:42 +0200

    add: new store method for categories

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/categoryController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/categoryController.java
@@ -1,12 +1,19 @@
 package org.lesson.java.spring.ticketplatform.controller;
 
 import org.lesson.java.spring.ticketplatform.model.Category;
+import org.lesson.java.spring.ticketplatform.model.Ticket;
 import org.lesson.java.spring.ticketplatform.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/categories")
@@ -31,5 +38,22 @@ public class categoryController {
 		
 		return "/categories/create";
 	}
+	
+	@PostMapping("/create")
+	public String store(
+			@Valid @ModelAttribute("category") Category formCategory,
+			BindingResult bindingResult,
+			Model model,
+			RedirectAttributes attributes) {
+		
+		if(bindingResult.hasErrors()) {
+			return "/categories/create";
+		}
+		
+		categoryService.saveCategory(formCategory);
+		
+		return "redirect:/categories";
+	}
+	
 
 }
