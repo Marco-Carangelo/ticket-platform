c91db9d32f94af77389e0cc9be699e1988e4d9b5
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-11 23:20:36 +0200

    add: new index method for categories

--- src/main/java/org/lesson/java/spring/ticketplatform/controller/categoryController.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/controller/categoryController.java
@@ -1,8 +1,11 @@
 package org.lesson.java.spring.ticketplatform.controller;
 
+import org.lesson.java.spring.ticketplatform.model.Category;
 import org.lesson.java.spring.ticketplatform.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +14,14 @@ public class categoryController {
 	
 	@Autowired
 	CategoryService categoryService;
+	
+	@GetMapping
+	public String index(Model model) {
+		
+		model.addAttribute("categoryList", categoryService.findCategories());
+		return "/categories/index";
+		
+	}
 	
 	
 
