d73cbf9cc40c5cd471cae800e92cad6997b6f5ca
Marco-Carangelo <165702051+Marco-Carangelo@users.noreply.github.com>
2024-10-12 21:27:55 +0200

    update: comments

--- src/main/java/org/lesson/java/spring/ticketplatform/repository/TicketRepository.java
+++ src/main/java/org/lesson/java/spring/ticketplatform/repository/TicketRepository.java
@@ -8,8 +8,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Integer>  {
 	
+	
+	//Method used to filter the tickets by ticket header given a substring
 	public List<Ticket> findByTicketHeaderContainsIgnoreCase(String text);
 	
+	//Method used to filter the tickets by status
 	public List<Ticket> findByTicketStatusEquals(Status status);
 	
 	
